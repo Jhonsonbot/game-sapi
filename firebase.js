b1c733b5fd0f7d5c143e1e9628f48c8db0b047c5
Onebl00d <103372972+Jhonsonbot@users.noreply.github.com>
2025-07-02 23:34:08 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyB8cKqCn3V9-0K54eY4xVwfoTf9yfVwURQ",
+  authDomain: "peternakan-sapi-c31df.firebaseapp.com",
+  projectId: "peternakan-sapi-c31df",
+  storageBucket: "peternakan-sapi-c31df.appspot.com",
+  messagingSenderId: "805090883196",
+  appId: "1:805090883196:web:a19acac871d994cb353bf9",
+  measurementId: "G-SGFRR6MJD7"
 };
 
 const app = initializeApp(firebaseConfig);
